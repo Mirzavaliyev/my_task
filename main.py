73d8ccd10f0cf6b2e92c24293e073e23daa811f6
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-01 00:35:14 +0500

    creating main.py
    I do my homeworks and save github

---
+++ main.py
@@ -0,0 +1,78 @@
+# favorite_numbers = {'sanjar': '23', 'tom': '1', 'Jamshid': '7'}
+# print(favorite_numbers
+#       )
+# glossary = {
+#     "variable": "A name that refers to a value stored in memory.",
+#     "function": "A block of organized, reusable code that performs a specific task.",
+#     "loop": "A sequence of instructions that repeats until a certain condition is met.",
+#     "list": "A collection of ordered, mutable items in Python.",
+#     "dictionary": "A collection of key-value pairs in Python."
+# }
+# variable = glossary['variable'].title()
+# print(variable)
+# function = glossary['function'].title()
+# print(function)
+# rivers = {'nile': 'egypt', 'amudaryo': 'uzbekistan', 'sirdaryo': 'uzbek'}
+# for river, country in rivers.items():
+#
+#     print(f"The {river} runs through {country}")
+# anvar = {'age': '26', 'high': '178', 'weight': '68'}
+# sardor = {'age': '32', 'high': '167', 'weight': '64'}
+#
+# new_list = ['age']
+# for i in anvar:
+#     if i in new_list:
+#         print(f"Thank you for polling {i}")
+#     else:
+#         print(f"{i} is not a valid choice")
+# person1 = {
+#     "first_name": "Alice",
+#     "last_name": "Smith",
+#     "age": 28,
+#     "city": "New York"
+# }
+#
+# person2 = {
+#     "first_name": "Bob",
+#     "last_name": "Johnson",
+#     "age": 34,
+#     "city": "Los Angeles"
+# }
+#
+# person3 = {
+#     "first_name": "Carol",
+#     "last_name": "Brown",
+#     "age": 25,
+#     "city": "Chicago"
+# }
+# people = [person1, person2, person3]
+# for person in people:
+#     print('\n Informatin about three people')
+#     for key, value in person.items():
+#         print(f"{key}: {value}")
+# animals = {'cat': 'Tom', 'dog': 'John', 'cow': 'Shavkatjon'}
+# keys = [animals['cat','dog','cow']]
+# for key in keys:
+#     print(key)
+# favorite_places = {
+# # 'john': ['newyork','liverpool','washington'],
+# # 'tom': ['brooklyn', 'samara','london'],
+# # 'michel': ['tashkent','samara','london']
+# # }
+# # for person, places in favorite_places.items():
+# #     print('About three people\'s favorite places:')
+# #     print(f"{person}'s favorite places are:")
+# #     for place in places:
+# #         print(f"{place}")
+# favorite_numbers = {'john': ['23','32','7'], 'tom': ['89','91','55']}
+# for keys, values in favorite_numbers.items():
+#     print('Two people\'s favorite numbers')
+#     print(f"{keys}'s favorite numbers are:")
+#     for valu in values:
+#             print(valu)
+# cities = {'newyork': ['modern', 'devoloped', 'beatiful']}
+# for city, definition in cities.items():
+#     print("About city")
+#     print(f"{city} is:")
+#     for i in definition:
+#         print(f"{i}")
