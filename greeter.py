2c83d3efb5c43d8c2e86d6f40f82fcc888357778
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-02 09:41:14 +0500

    бббб

---
+++ greeter.py
@@ -0,0 +1,16 @@
+# promt = "Write your name and we personilize you"
+# promt += "\n Write your name"
+# name = input(promt)
+# # print(name.title())
+# people = input('How many people come to party?')
+# people = int(people)
+# if people >= 8:
+#     print('Thera are no enough tables')
+# else:
+# #     print('Thera are enough tables')
+# number = input("Enter a number: ")
+# number = int(number)
+# if number % 10 == 0:
+#     print(f"{number} is divisible by 10")
+# else:
+#     print(f"{number} is not divisible by 10")
--- parrot.py
+++ parrot.py
@@ -1,3 +1,8 @@
-# While loop
-message = input("Enter your message and I will repeat ")
-print(message)
+# # While loop
+# message = input("Enter your message and I will repeat ")
+# print(message)
+prompt = "Say something and I will return your sentence"
+message = ""
+while message != "quit":
+    message = input(prompt)
+    print(message)
