fc644ce942ed745672439c9771f4ed73ccfd0872
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-04 17:24:09 +0500

    вщштп рщьуцщкл

---
+++ country.py
@@ -0,0 +1,9 @@
+def country_func(country, city):
+    all_c = f"{country},{city}"
+    return all_c
+pairs = country_func('Chine','Beijing')
+print(pairs)
+pairs1 = country_func('Russia', 'Moscow')
+print(pairs1)
+pairs2 = country_func('Uzbekistan', 'Namangan')
+print(pairs2)
---
+++ function.py
@@ -0,0 +1,28 @@
+# def info():
+#     """This function returns name and age"""
+#     name = input("write your name")
+#     age = int(input('when did you born'))
+#     ages = 2024 - age
+#     print(f"Hi {name} you are {ages} years old")
+#
+# info()
+# def sq_3rd():
+#     square = int(input("Write number that should be squared "))
+#     rd = int(input('Write number that should be 3 times '))
+#     square1 = square**2
+#     rd1 = rd**3
+#     print(f"sqaure is {square} 3 times is {rd1}")
+# sq_3rd()
+# def greet(username):
+#     print(f"Hello {username}")
+# greet("Shavkat")
+# def get_formatted_name(first_name,last_name):
+#     full_name = f"{first_name} {last_name}"
+#     return full_name
+# while True:
+#     print('write your full name')
+#     f_name = input('Write your name')
+#     l_name = input('Write your surname')
+#     ff = get_formatted_name(f_name, l_name)
+# print(ff)
+
