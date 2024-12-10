ba87c6edf8ff8a54f5e26db73ba13f9d95c6ca3a
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-10 11:48:46 +0500

    homework

--- repeating homeworks.py
+++ repeating homeworks.py
@@ -18,3 +18,39 @@
 # friends.append(8)
 # friends.remove('Shaxzamon')
 # print(friends)
+# friends = ['Bobur', 'Temur', 'Alisher']
+# friends.insert(0,"Javohir")
+# print(friends)
+# mehmonlar = []
+# friends = ['Bobur', 'Temur', 'Alisher']
+# mehmonlar.append(friends.pop(1))
+# print(mehmonlar)
+# friends = ['Bobur', 'Temur', 'Alisher']
+# # friends.sort()
+# print(sorted(friends))
+# fruits = ['pear','banana','apple','watermelon','lemon']
+# fruits.reverse()
+# print(fruits)
+# numbers = list(range(1,10,3))
+# print(numbers)
+# sonlar = [2323, 2222, 34333,9999]
+# arzon = min(sonlar)
+# qimmat = max(sonlar)
+# yigindi = sum(sonlar)
+# print(f" Eng arzon {arzon} Eng qimmat {qimmat} Jami {yigindi}")
+# cars = ['bmw','mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
+# car = cars[0:5]
+# print(car)
+# royhat = ['Saida', 'Javohir', 'Ramzan', 'Qizlarhon', 'Kamoliddin']
+# for i in royhat:
+#     print(f"Salom {i} yaxshimisz?")
+# bosh = []
+# for i in list(range(10,15)):
+#     bosh.append(i**3)
+#     print(bosh)
+# print(i)
+# empty = []
+# for i in range(5):
+#     empty.append(input(f"{i+1} sevimli filimingizni kiriting"))
+#     print(empty)
+
